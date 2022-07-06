7558e51cd94c6f562acabf36b8109e61b03242c1
Justin Sixsmith <justinsixsmith@gmail.com>
2022-07-06 10:38:07 -0500

    creates a RESTful API request

---
+++ src/main/java/Constants.java
@@ -0,0 +1,3 @@
+public class Constants {
+    public static final String API_KEY = "d8bd07dff5df461eaf0828056a6207c0";
+}
--- src/main/java/RestApiTutorial.java
+++ src/main/java/RestApiTutorial.java
@@ -1,12 +1,57 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class RestApiTutorial {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Transcript transcript = new Transcript();
+        transcript.setAudio_url("https://bit.ly/3yxKEIY");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
+
+        System.out.println(jsonRequest);
 
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-                .header("Authorization", )
+                .header("Authorization", Constants.API_KEY)
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(postResponse.body());
+
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+        System.out.println(transcript.getId());
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript" + transcript.getId()))
+                .header("Authorization", Constants.API_KEY)
+                .build();
+
+        while (true) {
+            HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            transcript = gson.fromJson(getResponse.body(), Transcript.class);
+
+            System.out.println(transcript.getStatus());
+
+            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Transcription completed.");
+        System.out.println(transcript.getText());
 
     }
 }
